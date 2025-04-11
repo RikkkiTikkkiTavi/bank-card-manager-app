81d081fbf0c91624e4b9748589aba67ec463c645
Constantine <Rikkki.Tikkki.Tavi@yandex.ru>
2025-04-11 17:58:06 +0300

    first

---
+++ src/main/java/com/example/bank_card_manager_app/BankCardManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_card_manager_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCardManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCardManagerAppApplication.class, args);
+	}
+
+}
